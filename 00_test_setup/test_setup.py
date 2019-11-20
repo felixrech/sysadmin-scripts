023f6b2e752ee8f6d596f03e96a56995fb262a2f
Felix Rech <mail@felixrech.com>
2019-11-20 20:24:28 +0100

    Refactor test_setup, add helper installation

--- 00_test_setup/test_setup.py
+++ 00_test_setup/test_setup.py
@@ -7,38 +7,75 @@ sys.path.append('../99_helpers/')
 from test_helpers import print_log, print_check, get_process_output  # noqa # pylint: disable=import-error
 
 
+# Constants
 python_version = 'python3.7'
 
-
-with open('test_config.json', 'r') as config_file:
-    config = json.load(config_file)
-
 # Hostname is vmXX where XX are the digits at the end of the Linux hostname
-# hostname = 'vm' + get_process_output('hostname')[-3:-1]
-hostname = 'vm01'
+hostname = 'vm' + get_process_output('hostname')[-3:-1]
+# Get full path to sysadmin-scripts folder
 dir = get_process_output('cd .. && pwd')[:-1] + '/'
+# Between tests we want to ask the user whether they want to continue and
+# clean the screen
 test_seperator = ("\nprintf \"\\n\\nPress a key to continue to next test\""
                   "&& read -n 1 -s && clear\n")
 
+# Read in the configuration file for which tests and helpers to install
+with open('test_config.json', 'r') as config_file:
+    config = json.load(config_file)
+
+
+def get_files_to_install(week, category):
+    """ Gets the files of given week and category.
+        Then filters to those to be installed locally
+    """
+    files = list(config[week][category].keys())
+    install_here = [hostname in config[week][category][f] for f in files]
+    files_to_install = [dir + f for f in list(compress(files, install_here))]
+    return files, files_to_install
+
+
+def write_script(name, files, seperator):
+    """ Writes a script that will execute the tests
+    """
+    # Set script's name and content
+    script_name = '/root/{0}'.format(name)
+    calls = ["{0} {1}".format(python_version, file) for file in files]
+    script_content = seperator.join(calls)
+    # Write the file and mark it executable
+    with open(script_name, 'w') as script_file:
+        script_file.write(script_content)
+    run("chmod +x {0}".format(script_name), shell=True)
+
+
+# TODO Set permissions of folder so only current user and root can access
+run("mkdir -p /root/helpers/", shell=True)
+
 for week in config:
     # Read which tests we need to install
-    tests = list(config[week]['files'].keys())
-    install_here = [hostname in config[week]['files'][test] for test in tests]
-    to_install = list(compress(tests, install_here))
-    # Prepare the root script's name and content
-    test_script = '/root/PSA_test{0}.sh'.format(config[week]['week'])
-    calls = ["{0} {1}{2}".format(python_version, dir, test)
-             for test in to_install]
-    test_script_content = test_seperator.join(calls)
+    tests, tests_to_install = get_files_to_install(week, 'tests')
+    script_name = config[week]['script_name']
+
     # Log what we're going to do in case of errors
-    print_log(
-        "Installing {0} of {1} file(s) for {2} into {3}".format(
-            len(to_install),
-            len(tests),
-            week,
-            test_script),
-        fill=75)
-    with open(test_script, 'w') as test_script_file:
-        test_script_file.write(test_script_content)
-    run("chmod +x {0}".format(test_script), shell=True)
+    log_msg = ("Installing {0} of {1} file(s) for {2} into {3}"
+               .format(len(tests_to_install), len(tests), week, script_name))
+    print_log(log_msg, fill=75)
+
+    # Install script
+    write_script(script_name, tests_to_install, test_seperator)
+    print_check(True)
+
+    # Some tests need helper, let's check whether this week needs one as well
+    if not 'helpers' in config[week].keys():
+        continue
+    _, helpers_to_install = get_files_to_install(week, 'helpers')
+    if len(helpers_to_install) == 0:
+        continue
+
+    # Install the helpers in the /root/helpers/ folder
+    log_msg = ("Installing {0} helper(s) for {1}"
+               .format(len(helpers_to_install), week))
+    print_log(log_msg, fill=75)
+    for helper in helpers_to_install:
+        script_name = 'helpers/{0}.sh'.format(helper.split('/')[-1][:-3])
+        write_script(script_name, [helper, ], '\n')
     print_check(True)
