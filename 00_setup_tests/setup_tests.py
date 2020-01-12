4294a3f387a0f7319ee3e5cc6dfd758ccfd4259b
Felix Rech <mail@felixrech.com>
2020-01-12 21:28:39 +0100

    Refactor setup_tests

--- 00_setup_tests/setup_tests.py
+++ 00_setup_tests/setup_tests.py
@@ -5,7 +5,7 @@ from itertools import compress
 
 sys.path.append(sys.path[0] + '/../99_helpers/')
 from test_helpers import get_vm_name  # noqa # pylint: disable=import-error
-from test_helpers import print_log, print_check, get_process_output  # noqa # pylint: disable=import-error
+from test_helpers import print_log, print_check, get_process_output, set_log_length  # noqa # pylint: disable=import-error
 
 
 # Constants
@@ -24,15 +24,27 @@ test_seperator = ("\nprintf \"\\n\\nPress a key to continue to next test\""
 with open('tests_config.json', 'r') as config_file:
     config = json.load(config_file)
 
+# Set log length to 50
+set_log_length(50)
+
 
 def get_files_to_install(week, category):
-    """ Gets the files of given week and category.
-        Then filters to those to be installed locally
+    """
+    Gets the files of given week and category filtered down to those to be
+    installed locally.
+
+    :param week: files of which week to look at (string, e.g. 'dns')
+    :param category: tests or helpers (string)
+    :returns: list of source files (list of strings)
     """
     files = list(config[week][category].keys())
     install_here = [hostname in config[week][category][f] for f in files]
     files_to_install = [dir + f for f in list(compress(files, install_here))]
-    return files, files_to_install
+    return files_to_install
+
+
+def extract_name(test_name):
+    return test_name.split('_')[-1]
 
 
 def write_script(name, files, seperator):
@@ -40,7 +52,8 @@ def write_script(name, files, seperator):
     """
     # Set script's name and content
     script_name = '/root/{0}'.format(name)
-    calls = ["{0} {1}".format(python_version, file) for file in files]
+    files = list(map(extract_name, files))
+    calls = ["{0} /root/tests/{1}".format(python_version, file) for file in files]
     script_content = seperator.join(calls)
     # Write the file and mark it executable
     with open(script_name, 'w') as script_file:
@@ -48,55 +61,53 @@ def write_script(name, files, seperator):
     run("chmod +x {0}".format(script_name), shell=True)
 
 
-def install_for_automated_testing(tests):
-    if len(tests) == 0:
-        return
-    print_log(("Installing {0} file(s) for automated testing"
-               .format(len(tests))), fill=75)
-    base = "/root/tests/"
-    for test in tests:
-        cmd = "ln -sf {0} {1}".format(test, base + test.split('_')[-1])
-        run(cmd, shell=True)
+def install_files(files, destination):
+    for file_name in files:
+        # '/.../test_ldap.py' -> '<destination>/ldap.py
+        install_name = destination + '/' + extract_name(file_name)
+        # Copy file to destination
+        run("cp {0} {1}".format(file_name, install_name), shell=True)
+
+
+def install_tests(tests, week):
+    print_log("Installing {0} test file(s) for {1}".format(len(tests), week))
+    install_files(tests, '/root/tests')
     print_check(True)
 
 
-# TODO Set permissions of folder so only current user and root can access
-run("mkdir -p /root/tests", shell=True)
-run("mkdir -p /root/helpers/", shell=True)
-
-for week in config:
-    # Read which tests we need to install
-    tests, tests_to_install = get_files_to_install(week, 'tests')
-    script_name = config[week]['script_name']
-
-    # Log what we're going to do in case of errors
-    log_msg = ("Installing {0} of {1} file(s) for {2} into {3}"
-               .format(len(tests_to_install), len(tests), week, script_name))
-    print_log(log_msg, fill=75)
-
-    # Install script
-    if len(tests_to_install) >= 1:
-        write_script(script_name, tests_to_install, test_seperator)
+def install_helpers(helpers, week):
+    msg = "Installing {0} helper file(s) for {1}".format(len(helpers), week)
+    print_log(msg)
+    install_files(helpers, '/root/helpers')
     print_check(True)
 
-    # Install tests for automatic testing
-    install_for_automated_testing(tests_to_install)
 
-    # Some tests need helper, let's check whether this week needs one as well
-    if not 'helpers' in config[week].keys():
-        continue
-    _, helpers_to_install = get_files_to_install(week, 'helpers')
-    if len(helpers_to_install) == 0:
-        continue
+if __name__ == "__main__":
+    # Setup folders
+    run("mkdir -p /root/tests", shell=True)
+    run("mkdir -p /root/helpers/", shell=True)
 
-    # Install the helpers in the /root/helpers/ folder
-    log_msg = ("Installing {0} helper(s) for {1}"
-               .format(len(helpers_to_install), week))
-    print_log(log_msg, fill=75)
-    for helper in helpers_to_install:
-        cmd = "ln -sf {0} /root/helpers/{1}"
-        run(cmd.format(helper, helper.split('/')[-1]), shell=True)
-    print_check(True)
+    # Copy test_helpers
+    cmd = "cp ../99_helpers/test_helpers.py /root/{}/test_helpers.py"
+    run(cmd.format('tests'), shell=True)
+    run(cmd.format('helpers'), shell=True)
 
-    # Install the helpers as tests for automatic testing
-    install_for_automated_testing(helpers_to_install)
+    for week in config:
+        # Read which tests we need to install
+        tests_to_install = get_files_to_install(week, 'tests')
+        script_name = config[week]['script_name']
+
+        # Install tests and helpers
+        if len(tests_to_install) > 0:
+            install_tests(tests_to_install, week)
+
+        # Install script
+        if len(tests_to_install) >= 1:
+            write_script(script_name, tests_to_install, test_seperator)
+
+        # Some tests need helpers
+        if not 'helpers' in config[week].keys():
+            continue
+        helpers_to_install = get_files_to_install(week, 'helpers')
+        if len(helpers_to_install) > 0:
+            install_helpers(helpers_to_install, week)
