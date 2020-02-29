1a23809af1d41da9ca2b3ace3ee5426ab1e7bee6
Felix Rech <mail@felixrech.com>
2020-02-29 13:36:47 +0100

    Update network test to work on all VMs and deploy them

--- 02_network/check_network.py
+++ 02_network/check_network.py
@@ -1,26 +1,50 @@
 #!/usr/bin/env python3.7
 
+import threading
 from sys import stdout
+from time import sleep
 from subprocess import run, Popen, PIPE
 
 import sys
 sys.path.append(sys.path[0] + '/../99_helpers/')
-from test_helpers import print_log, print_check  # noqa # pylint: disable=import-error
+from test_helpers import get_process_output, get_process_returncode, get_vm_name  # noqa # pylint: disable=import-error
+from test_helpers import print_log, print_check, print_crit_check, set_log_length  # noqa # pylint: disable=import-error
 from test_helpers import print_test_summary  # noqa # pylint: disable=import-error
-from test_helpers import get_process_output, get_process_returncode  # noqa # pylint: disable=import-error
 
-own_ip = '192.168.10.1'
 team_test_ip = '192.168.10.2'
 team_router_ip = team_test_ip
 praktikum_test_ip = '192.168.178.9.1'
 proxy_ip = '131.159.0.2'
 
 
+set_log_length(65)
+
+
+def is_port_open(ip, port):
+    cmd = f"nc -w5 -z {ip} {port}"
+    return get_process_returncode(cmd) == 0
+
+
+def is_local_port_open(port, run_server=False):
+    def server():
+        process = Popen(f"exec nc -l {port}", shell=True)
+        sleep(30)
+        process.kill()
+        process.terminate()
+    if run_server:
+        t = threading.Thread(target=server)
+        t.start()
+    cmd = f"ssh testing-vm nc -w5 -z 192.168.10.1 {port}"
+    cond = get_process_returncode(cmd) == 0
+    return cond
+
+
 # Check IP address
 print_log("[NETWORK] IP address configured")
-cmd = "ip addr show enp0s8"
-configuration = get_process_output(cmd)
-print_check("inet {0}/24".format(own_ip) in configuration)
+ip = get_process_output("ip -o -f inet addr show dev enp0s8")
+print_crit_check("inet 192.168.10." in ip)
+print_log("[NETWORK] Checking subnet mask")
+print_check("/24 brd" in ip)
 
 # Check team connection
 print_log("[NETWORK] team subnet reachable")
@@ -34,10 +58,21 @@ print_check(get_process_returncode(cmd) == 0)
 
 # Check routes
 print_log("[NETWORK] routes")
-cmd = "ip route show"
-routes = get_process_output(cmd)
-cond_check = "192.168.0.0/16 via {0}".format(team_router_ip) in routes
-print_check(cond_check)
+routes = get_process_output("ip route show | grep via | grep 192")
+if get_vm_name() == "vm02":
+    cond = ("192.168.1.0/24 via 192.168.101.1" in routes and
+            "192.168.2.0/24 via 192.168.102.2" in routes and
+            "192.168.3.0/24 via 192.168.103.3" in routes and
+            "192.168.4.0/24 via 192.168.104.4" in routes and
+            "192.168.5.0/24 via 192.168.105.5" in routes and
+            "192.168.6.0/24 via 192.168.106.6" in routes and
+            "192.168.7.0/24 via 192.168.107.7" in routes and
+            "192.168.8.0/24 via 192.168.108.8" in routes and
+            "192.168.9.0/24 via 192.168.109.9" in routes and
+            "192.168.11.0/24 via 192.168.120.11" in routes)
+else:
+    cond = "192.168.0.0/16 via {0}".format(team_router_ip) in routes
+print_check(cond)
 
 # Check some firewall rules
 cmd = "iptables -nL {0}"
@@ -49,27 +84,44 @@ raw_out_tables = get_process_output(cmd.format('OUTPUT -t raw'))
 print_log("[FIREWALL] policy drop")
 cond = "policy DROP" in in_tables and "policy DROP" in out_tables
 print_check(cond)
-print_log("[FIREWALL] incoming ssh allowed")
-cond1 = "ACCEPT     tcp  --  0.0.0.0/0            0.0.0.0/0            tcp spt:22 ctstate RELATED,ESTABLISHED" in out_tables
-cond2 = "ACCEPT     tcp  --  0.0.0.0/0            0.0.0.0/0            tcp dpt:22" in in_tables
-print_check(cond1 and cond2)
-print_log("[FIREWALL] outgoing ssh allowed")
-cmd = "ssh -o StrictHostKeyChecking=no github.com"
-print_check(get_process_returncode(cmd) == 255)
+print_log("[FIREWALL] Port 123 closed")
+print_check(not is_local_port_open(123, run_server=True))
+print_log("[FIREWALL] Port 1234 closed")
+print_check(not is_local_port_open(1234, run_server=True))
+
+print_log("[FIREWALL] proxy connections possible")
+print_check(is_port_open('131.159.0.2', 8080))
+
+print_log("[FIREWALL] Incoming SSH possible")
+print_check(is_local_port_open(22))
+print_log("[FIREWALL] outgoing ssh possible (PSA network)")
+print_check(is_port_open('192.168.8.1', 22))
+print_log("[FIREWALL] outgoing ssh possible (faculty network)")
+print_check(is_port_open('131.159.74.56', 22))
+print_log("[FIREWALL] outgoing ssh not possible (outside faculty & PSA)")
+print_check(not is_port_open('140.82.218.3', 22))
+
+print_log("[FIREWALL] Port 80 open")
+print_check(is_local_port_open(80))
+print_log("[FIREWALL] Port 443 open")
+print_check(is_local_port_open(443))
+
 print_log("[FIREWALL] icmp allowed")
 cond1 = "ACCEPT     icmp --  0.0.0.0/0            0.0.0.0/0" in out_tables
 cond2 = "ACCEPT     icmp --  0.0.0.0/0            0.0.0.0/0" in in_tables
 print_check(cond1 and cond2)
-print_log("[FIREWALL] proxy connections allowed")
-cond1 = "ACCEPT     tcp  --  131.159.0.2          0.0.0.0/0            tcp dpt:8080" in out_tables
-cond2 = "ACCEPT     tcp  --  0.0.0.0/0            131.159.0.2          tcp spt:8080 ctstate RELATED,ESTABLISHED" in in_tables
-print_check(cond1 and cond2)
+print_log("[FIREWALL] ICMP out possible")
+print_check(get_process_returncode("ping 8.8.8.8 -c 5 -i 0") == 0)
+print_log("[FIREWALL] ICMP in possible")
+print_check(get_process_returncode("ssh testing-vm ping 8.8.8.8 -c 3") == 0)
+
 print_log("[FIREWALL] NOTRACK active")
 cond1 = ("tcp  --  0.0.0.0/0            0.0.0.0/0            tcp dpt:80 NOTRACK" in raw_in_tables and
          "tcp  --  0.0.0.0/0            0.0.0.0/0            tcp dpt:443 NOTRACK" in raw_in_tables)
 cond2 = ("tcp  --  0.0.0.0/0            0.0.0.0/0            tcp spt:80 NOTRACK" in raw_out_tables and
          "tcp  --  0.0.0.0/0            0.0.0.0/0            tcp spt:443 NOTRACK" in raw_out_tables)
 print_check(cond1 and cond2)
+
 
 # Check IP address using http and https (is proxy connected?)
 # Sourcing the bashrc and setting of proxy environment variables happens
